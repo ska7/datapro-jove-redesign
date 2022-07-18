44e023e88447d58a31ce443161d31b78dfa77bc8
Nick Aralin <nikkofann@gmail.com>
2022-07-18 09:58:29 -0400

    add content description section

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
       colors: {
         "navbar-bg": "#0F464E",
         "navbar-btn": "#0EC5B4",
+        "banner-title-green": "#0A9B8D",
         "inactive-card-green": "rgba(10, 155, 141, 0.7)",
         gray: "#909AA4",
       },
@@ -23,6 +24,22 @@ module.exports = {
         8: "8",
         9: "9",
         10: "10",
+      },
+      screens: {
+        "xs-max": { max: "639px" },
+        "sm-max": { max: "767px" },
+        "md-max": { max: "1023px" },
+        "lg-max": { max: "1279px" },
+        "xl-max": { max: "1535px" },
+        sm: { min: "640px", max: "767px" },
+
+        md: { min: "768px", max: "1023px" },
+
+        lg: { min: "1024px", max: "1279px" },
+
+        xl: { min: "1280px", max: "1535px" },
+
+        "2xl": { min: "1536px" },
       },
     },
   },
